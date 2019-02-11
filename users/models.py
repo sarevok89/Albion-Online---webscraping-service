32e3bdf1d339a1964df00ef094226eec20586ecc
sarevok89 <sarevok1989@gmail.com>
2019-02-11 15:40:48 +0100

    modified profile model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from io import BytesIO
+import sys
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 class Profile(models.Model):
@@ -11,11 +14,31 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     def save(self, *args, **kwargs):
+        # Opening the uploaded image
+        im = Image.open(self.image)
+
+        output = BytesIO()
+
+        # Resize/modify the image
+        output_size = (300, 300)
+        im = im.resize(output_size)
+
+        # after modifications, save it to the output
+        im.save(output, format='JPEG', quality=90)
+        output.seek(0)
+
+        # change the imagefield value to be the newley modifed image value
+        self.image = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg',
+                                          sys.getsizeof(output), None)
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.name).convert('RGB')
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.name).convert('RGB')
+    #
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.name)
