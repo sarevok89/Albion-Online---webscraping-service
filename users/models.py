363d8f088d61dc1f47119cbe185ceea7f9f82009
sarevok89 <sarevok1989@gmail.com>
2019-02-11 14:53:01 +0100

    changed path to name in profile model

--- users/models.py
+++ users/models.py
@@ -13,9 +13,9 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path).convert('RGB')
+        img = Image.open(self.image.name).convert('RGB')
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.save(self.image.name)
