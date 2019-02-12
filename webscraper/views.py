48dbe1a90ebed0f4236eb049077376165961c36e
sarevok89 <sarevok1989@gmail.com>
2019-02-12 15:33:37 +0100

    fixed a typo in viesw.py access key

--- webscraper/views.py
+++ webscraper/views.py
@@ -37,7 +37,8 @@ class WebscraperView(View):
 
             temp_file = os.path.join(BASE_DIR, 'webscraper', 'temp', file_name)
 
-            s3 = boto3.resource('s3', aws_secret_key_id='AKIAJZ7G7LLNHVOEGTKA', aws_secret_access_key='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')
+            s3 = boto3.resource('s3', aws_access_key_id='AKIAJZ7G7LLNHVOEGTKA',
+                                aws_secret_access_key='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')
             s3.meta.client.upload_file(temp_file, 'albion-compensations', file_name)
 
             obj.excel_file.name = os.path.join(MEDIA_ROOT, 'compensations', file_name)
