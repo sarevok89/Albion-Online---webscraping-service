4524e8a9336bf0ef328c80cbfb544cd64eafe944
sarevok89 <sarevok1989@gmail.com>
2019-02-12 15:53:45 +0100

    changed paths in views.py

--- webscraper/views.py
+++ webscraper/views.py
@@ -39,9 +39,9 @@ class WebscraperView(View):
 
             s3 = boto3.resource('s3', aws_access_key_id='AKIAJZ7G7LLNHVOEGTKA',
                                 aws_secret_access_key='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')
-            s3.meta.client.upload_file(file, 'albion-compensations', file_name)
+            s3.meta.client.upload_file(temp_file, 'albion-compensations', MEDIA_ROOT + 'compensations/', file_name)
 
-            obj.excel_file.name = os.path.join(MEDIA_ROOT, 'compensations', file_name)
+            obj.excel_file.name = MEDIA_ROOT + 'compensations/' + file_name
             obj.save()
 
             context = {
