f330d5ed439852991d303627a35647a1eeb61a49
sarevok89 <sarevok1989@gmail.com>
2019-02-13 11:55:25 +0100

    reversed 2 previous changes

--- webscraper/static/webscraper/killboard_app.py
+++ webscraper/static/webscraper/killboard_app.py
@@ -7,7 +7,7 @@ import datetime
 import json
 import os
 import re
-from albion_compensations.settings import BASE_DIR, MEDIA_LOCAL_ROOT
+from albion_compensations.settings import BASE_DIR, MEDIA_ROOT
 import boto3
 
 
@@ -227,7 +227,7 @@ def generate_excel(dict_list, fight_name):
     objs = list(bucket.objects.filter(Prefix='compensations/' + file_name))
 
     if len(objs) == 0 or objs[0].key != file_name:
-        writer = ExcelWriter(os.path.join(MEDIA_LOCAL_ROOT, 'compensations', file_name))
+        writer = ExcelWriter(os.path.join(MEDIA_ROOT, 'compensations', file_name))
     else:
         while True:
             if len(objs) > 0 and objs[0].key == file_name:
@@ -235,7 +235,7 @@ def generate_excel(dict_list, fight_name):
                 file_name = current_date + fight_name + f'({num_of_files}).xlsx'
                 continue
             else:
-                writer = ExcelWriter(os.path.join(MEDIA_LOCAL_ROOT, 'compensations', file_name))
+                writer = ExcelWriter(os.path.join(MEDIA_ROOT, 'compensations', file_name))
                 break
 
     df.to_excel(writer, 'Sheet 1', header=False)
