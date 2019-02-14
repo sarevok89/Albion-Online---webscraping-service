c988f61876e846aea77460acd37eb54b5499e975
sarevok89 <sarevok1989@gmail.com>
2019-02-14 14:56:35 +0100

    Gitignore changes

--- Virtual/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sarevok89\pycharmprojects\django - tutorial\django_basics\albion_compensations\virtual\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
