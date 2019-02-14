ec74b6e15e57168bbb441f3aa59fb77791061e66
sarevok89 <sarevok1989@gmail.com>
2019-02-14 12:48:06 +0100

    Editted admin panel view, added downloadable links for each killboard in list view.

--- webscraper/admin.py
+++ webscraper/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Killboard, Post
+from django.utils.safestring import mark_safe
+
+
+class KillboardAdmin(admin.ModelAdmin):
+    def file_link(self, obj):
+        return mark_safe('<a href="%s">Download</a>' % obj.excel_file)
+
+    file_link.allow_tags = True
+    list_display = ('date', 'user', 'fight_name', 'file_link')
+    list_filter = ('date', 'user')
+
 
 admin.site.register(Post)
-admin.site.register(Killboard)
+admin.site.register(Killboard, KillboardAdmin)
